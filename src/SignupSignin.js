ef31d6db5f97c391bc53da22076d0dc679d34025
pranotobudi <pranotobudi.s@gmail.com>
2019-03-01 13:53:03 +0700

    add isSignupSuccess state to SignupSignin Component to render Signup success message

--- src/SignupSignin.js
+++ src/SignupSignin.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Alert } from 'antd';
 import SignupSigninHeader from './SignupSigninHeader';
 import SignupSigninLeftContent from './SignupSigninLeftContent';
 import SignupSigninRightContent from './SignupSigninRightContent';
 
-const SignupSignin = () => (
-    <div className="signup-signin">
-        <div>
-            <SignupSigninHeader />
-        </div>
-        <div className="signup-signin-main-content">
-            <div className="signup-signin-left-content">
-                <SignupSigninLeftContent />
+class SignupSignin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSignupSucess: false,
+        };
+    }
+    render() {
+        const isSignupSucess = this.state.isSignupSucess;
+        return (
+            <div className="signup-signin">
+                <div>
+                    <SignupSigninHeader />
+                </div>
+                {isSignupSucess ? (
+                    <div>
+                        <Alert
+                            message="Sign Up Success, you can log in now!"
+                            style={{ textAlign: 'center' }}
+                            type="success"
+                        // showIcon
+                            closable />
+                    </div>
+
+                    ) : (
+                        <div />
+                )}
+                <div className="signup-signin-main-content">
+                    <div className="signup-signin-left-content">
+                        <SignupSigninLeftContent />
+                    </div>
+                    <div className="signup-signin-right-content">
+                        <SignupSigninRightContent />
+                    </div>
+                </div>
             </div>
-            <div className="signup-signin-right-content">
-                <SignupSigninRightContent />
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 
 export default SignupSignin;
