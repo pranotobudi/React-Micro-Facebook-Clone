633e825e730498f6dbdeea62cd062263558ed102
pranotobudi <pranotobudi.s@gmail.com>
2019-03-01 17:37:44 +0700

    add alertHandler for Login and Signup

--- src/SignupSignin.js
+++ src/SignupSignin.js
@@ -11,10 +11,29 @@ class SignupSignin extends Component {
             showSignupAlert: false,
             signupAlertMessage: 'Sign Up Success, you can log in now!',
             signupAlertType: 'info',
+            showLoginAlert: false,
+            loginAlertMessage: 'Log in Failed, please check again your email and password!',
+            loginAlertType: 'error',
         };
 
         this.updateSignupStateHandler = this.updateSignupStateHandler.bind(this);
+        this.updateLoginStateHandler = this.updateLoginStateHandler.bind(this);
+        this.loginAlertOnCloseHandler = this.loginAlertOnCloseHandler.bind(this);
+        this.signupAlertOnCloseHandler = this.signupAlertOnCloseHandler.bind(this);
     }
+
+    signupAlertOnCloseHandler() {
+        this.setState({
+            showSignupAlert: false,
+        });
+    }
+
+    loginAlertOnCloseHandler() {
+        this.setState({
+            showLoginAlert: false,
+        });
+    }
+
     updateSignupStateHandler(showSignupAlertValue, signupAlertMessageValue, signupAlertTypeValue) {
         this.setState({
             showSignupAlert: showSignupAlertValue,
@@ -22,12 +41,20 @@ class SignupSignin extends Component {
             signupAlertType: signupAlertTypeValue,
         });
     }
+    updateLoginStateHandler(showLoginAlertValue, loginAlertMessageValue, loginAlertTypeValue) {
+        this.setState({
+            showLoginAlert: showLoginAlertValue,
+            loginAlertMessage: loginAlertMessageValue,
+            loginAlertType: loginAlertTypeValue,
+        });
+    }
     render() {
         const showSignupAlert = this.state.showSignupAlert;
+        const showLoginAlert = this.state.showLoginAlert;
         return (
             <div className="signup-signin">
                 <div>
-                    <SignupSigninHeader />
+                    <SignupSigninHeader updateLoginStateHandler={this.updateLoginStateHandler} />
                 </div>
                 {showSignupAlert ? (
                     <div>
@@ -36,12 +63,28 @@ class SignupSignin extends Component {
                             style={{ textAlign: 'center' }}
                             type={this.state.signupAlertType}
                         // showIcon
-                            closable />
+                            closable
+                            onClose={this.signupAlertOnCloseHandler} />
                     </div>
 
                     ) : (
                         <div />
                 )}
+                {showLoginAlert ? (
+                    <div>
+                        <Alert
+                            message={this.state.loginAlertMessage}
+                            style={{ textAlign: 'center' }}
+                            type={this.state.loginAlertType}
+                        // showIcon
+                            closable
+                            onClose={this.loginAlertOnCloseHandler} />
+                    </div>
+
+                    ) : (
+                        <div />
+                )}
+
                 <div className="signup-signin-main-content">
                     <div className="signup-signin-left-content">
                         <SignupSigninLeftContent />
